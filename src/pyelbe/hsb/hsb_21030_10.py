97700cb41bfe4a286d835a4ed8a03241e717de6b
Kris Koppes <kriskoppes@hotmail.com>
2022-09-14 10:02:15 +0200

    added markdown string generation

--- src/pyelbe/hsb/hsb_21030_10.py
+++ src/pyelbe/hsb/hsb_21030_10.py
@@ -35,7 +35,7 @@ def moment_x_reference_markdown(
     """
     Create markdown in LaTeX for moment_x_reference
     args:
-        m_moment_x_px_p(float): moment of the force
+        moment_x_p(float): moment of the force
                                 about the point of application in the x direction
         force_y(float): force in the y direction
         force_z(float): force in the z direction
@@ -81,5 +81,43 @@ def moment_y_reference(
         moment_myu(float): moment of the force about the point of application in the y direction
     """
 
-    moment_y_ref = moment_y_p - force_x * z_coord_p + force_z * x_coord_p
+    moment_y_ref = moment_y_p + force_x * z_coord_p - force_z * x_coord_p
+
     return float(moment_y_ref)
+
+def moment_y_reference_markdown(
+    moment_y_p: float,
+    force_x: float,
+    force_z: float,
+    x_coord_p: float,
+    z_coord_p: float,
+) -> str:
+    """
+    Create markdown in LaTeX for moment_y_reference
+    args:
+        moment_y_p(float): moment of the force
+                                about the point of application in the y direction
+        force_x(float): force in the x direction
+        force_z(float): force in the z direction
+        x_coord_p(float): x coordinate of the point of application of the force
+        z_coord_p(float): z coordinate of the point of application of the force
+    returns:
+        markdown(str): markdown in LaTeX for moment_y_reference
+    """
+
+    moment_y_ref = moment_y_reference(
+        moment_y_p, force_x, force_z, x_coord_p, z_coord_p
+    )
+    # for LaTeX
+    # pylint: disable=anomalous-backslash-in-string
+    markdown_formula = """$$
+    M_{yU} = M_{yP} + F_{x}\cdot z_{P} - F_{z}\cdot x_{P}
+    $$
+    """
+
+    markdown_filled = (f"$${moment_y_ref} = "
+    f"{moment_y_p} + {force_x} \cdot {z_coord_p} - {force_z} \cdot {x_coord_p}$$")
+
+    # pylint: enable=anomalous-backslash-in-string
+
+    return markdown_formula, markdown_filled
